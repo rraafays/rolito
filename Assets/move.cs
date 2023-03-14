192cb83615fab152a6acb1214df4c16db3707541
raf <rraf@tuta.io>
2023-03-14 14:45:48 +0000

    update: flappy bird type demo

--- Assets/move.cs
+++ Assets/move.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class move : MonoBehaviour
 {
     public float movement_speed = 5;
+    public float kill_zone = -30;
 
     void Start()
     {
@@ -14,5 +15,6 @@ public class move : MonoBehaviour
     void Update()
     {
         transform.position = transform.position + (Vector3.left * movement_speed) * Time.deltaTime;
+        if (transform.position.x < kill_zone) { Destroy(gameObject); }
     }
 }
--- Assets/spawner.cs
+++ Assets/spawner.cs
@@ -7,11 +7,12 @@ public class spawner : MonoBehaviour
     public GameObject prefab;
     public float spawn_rate = 2;
     private float timer = 0;
+    public float y_offset = 10;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawn();
     }
 
     // Update is called once per frame
@@ -23,8 +24,16 @@ public class spawner : MonoBehaviour
         }
         else
         {
-            Instantiate(prefab, transform.position, transform.rotation);
+            spawn();
             timer = 0;
         }     
+    }
+
+    void spawn() 
+    { 
+        float y_minimum = transform.position.y - y_offset;
+        float y_maximum = transform.position.y + y_offset;
+
+        Instantiate(prefab, new Vector3(transform.position.x, Random.Range(y_minimum, y_maximum), 0), transform.rotation); 
     }
 }
