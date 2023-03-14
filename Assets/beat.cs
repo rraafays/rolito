b1287e857d8eee76fdf2fef9002bfadd8108469e
raf <rraf@tuta.io>
2023-03-14 12:56:20 +0000

    update: demo of game reacting to a beat

---
+++ Assets/beat.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beat : MonoBehaviour
+{
+    [SerializeField] bool use_test_beat;
+    [SerializeField] float pulse_size = 1.15f;
+    [SerializeField] float return_speed = 5f;
+    private Vector3 start_size;
+
+    void Start()
+    {
+        start_size = transform.localScale;
+        if (use_test_beat) { StartCoroutine(test_beat()); }
+    }
+
+    void Update() { transform.localScale = Vector3.Lerp(transform.localScale, start_size, Time.deltaTime * return_speed); }
+
+    public void pulse() { transform.localScale = start_size * pulse_size; }
+
+    IEnumerator test_beat()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            pulse();
+        }
+    }
+}
---
+++ Assets/beat_controller.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class beat_controller : MonoBehaviour
+{
+    [SerializeField] private float bpm;
+    [SerializeField] private AudioSource audio_source;
+    [SerializeField] private Interval[] intervals;
+
+    private void Update()
+    {
+        foreach (Interval interval in intervals)
+        {
+            float sampled_time = (audio_source.timeSamples / (audio_source.clip.frequency * interval.get_beat_length(bpm)));
+            interval.check_for_new_interval(sampled_time);
+        }
+    }
+}
+
+[System.Serializable]
+public class Interval
+{
+    [SerializeField] private float steps;
+    [SerializeField] private UnityEvent trigger;
+    private int last_interval;
+
+    public float get_beat_length(float bpm) { return 60f / (bpm * steps); }
+
+    public void check_for_new_interval (float interval)
+    {
+        if (Mathf.FloorToInt(interval) != last_interval) 
+        { 
+            last_interval = Mathf.FloorToInt(interval); 
+            trigger.Invoke();
+        }
+    }
+}
