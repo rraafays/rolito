a5476cfaca3b5baa9a257cf3ef9aa23be47c4a2f
raf <rraf@tuta.io>
2023-03-15 21:26:05 +0000

    update: varying qualities of pata

--- Assets/scripts/drum.cs
+++ Assets/scripts/drum.cs
@@ -6,12 +6,16 @@ using UnityEngine.Events;
 public class drum : MonoBehaviour
 {
   [SerializeField] private AudioSource audio_source;
-  public AudioClip perfect_pata;
-  public AudioClip good_pata;
-  public AudioClip bad_pata;
-  private float perfect_window;
-  private float second = 0.1f;
-  private bool expect = false;
+  private bool expect_perfect = false;
+  private bool expect_good = false;
+  private bool expect_bad = false;
+  private float perfect_window = 0.1f;
+  private float good_window = 0.3f;
+  private float bad_window = 0.6f;
+  private bool perfect_pata;
+  public AudioClip perfect_pata_sound;
+  public AudioClip good_pata_sound;
+  public AudioClip bad_pata_sound;
 
   private void Start()
   {
@@ -19,24 +23,39 @@ public class drum : MonoBehaviour
 
   private void Update()
   {
-    if(expect)
+    if (expect_perfect)
     {
-      second -= Time.smoothDeltaTime;
-      if (second >= 0)
-      {
-        if (Input.GetKeyDown(KeyCode.F) == true) { audio_source.PlayOneShot(perfect_pata); }
-      }
-      else
-      {
-        second = 0.1f;
-        expect = false;
-      }
+      perfect_window -= Time.smoothDeltaTime;
+      if (perfect_window >= 0) { expect_pata('p'); return; }
+      else { perfect_window = 0.1f; expect_perfect = false; expect_good = true; }
+    }
+    if (expect_good)
+    {
+      good_window -= Time.smoothDeltaTime;
+      if (good_window >= 0) { expect_pata('g'); return; }
+      else { good_window = 0.3f; expect_good = false; expect_bad = true; }
+    }
+    if (expect_bad)
+    {
+      bad_window -= Time.smoothDeltaTime;
+      if (bad_window >= 0) { expect_pata('b'); return; }
+      else { bad_window = 0.6f; expect_bad = false; }
     }
   }
 
-  public void expect_pata()
+  public void expect_drum()
   {
-    expect = true;
-    if (Input.GetKey(KeyCode.F) == true) { audio_source.PlayOneShot(perfect_pata); }
+    expect_perfect = true;
+    expect_pata('p');
+  }
+
+  void expect_pata(char c) 
+  { 
+    if (Input.GetKeyDown(KeyCode.F) == true) 
+    { 
+      if (c == 'p') { audio_source.PlayOneShot(perfect_pata_sound); }
+      if (c == 'g') { audio_source.PlayOneShot(good_pata_sound); }
+      if (c == 'b') { audio_source.PlayOneShot(bad_pata_sound); }
+    } 
   }
 }
