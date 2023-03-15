aa17a840f5737814779e8a0e195fd0d7b4847814
raf <rraf@tuta.io>
2023-03-15 09:09:46 +0000

    chore: refactoring because c# is ugly

--- Assets/scripts/flag_unit.cs
+++ Assets/scripts/flag_unit.cs
@@ -4,38 +4,38 @@ using UnityEngine;
 
 public class flag_unit : MonoBehaviour
 {
-    [SerializeField] bool use_test_beat;
-    [SerializeField] float beat_size = 1.15f;
-    [SerializeField] float return_speed = 5f;
-    public Rigidbody2D body;
-    private Vector3 start_size;
+  [SerializeField] bool use_test_beat;
+  [SerializeField] float beat_size = 1.15f;
+  [SerializeField] float return_speed = 5f;
+  public Rigidbody2D body;
+  private Vector3 start_size;
 
-    void Start()
+  void Start()
+  {
+    start_size = transform.localScale;
+    if (use_test_beat) { StartCoroutine(test_beat()); }
+  }
+
+  void Update() 
+  { 
+    transform.localScale = Vector3.Lerp(transform.localScale, start_size, Time.deltaTime * return_speed); 
+    if (Input.GetKeyDown(KeyCode.Space) == true)
     {
-        start_size = transform.localScale;
-        if (use_test_beat) { StartCoroutine(test_beat()); }
+      body.velocity = Vector2.up * 3;
     }
+  }
 
-    void Update() 
-    { 
-      transform.localScale = Vector3.Lerp(transform.localScale, start_size, Time.deltaTime * return_speed); 
-      if (Input.GetKeyDown(KeyCode.Space) == true)
-      {
-        body.velocity = Vector2.up * 3;
-      }
-    }
+  public void beat() 
+  { 
+    transform.localScale = start_size * beat_size; 
+  }
 
-    public void beat() 
-    { 
-      transform.localScale = start_size * beat_size; 
-    }
-
-    IEnumerator test_beat()
+  IEnumerator test_beat()
+  {
+    while (true)
     {
-        while (true)
-        {
-            yield return new WaitForSeconds(1f);
-            beat();
-        }
+      yield return new WaitForSeconds(1f);
+      beat();
     }
+  }
 }
