9184137e21f33e30c2a2517d8063bc9599b711a7
raf <rraf@tuta.io>
2023-04-16 14:05:37 +0100

    update: slight refactoring

--- Assets/scripts/drum.cs
+++ Assets/scripts/drum.cs
@@ -9,30 +9,23 @@ public class drum : MonoBehaviour {
   public AudioSource speaker;
   public string command;
 
-  private enum Sound {
-    Pata,
-    Pon,
-    Don,
-    Chaka
-  }
-
+  private enum Name { Pata, Pon, Don, Chaka }
   private struct Drum {
-    public Sound sound;
+    public Name name;
     public KeyCode bind;
 
-    public Drum(Sound sound, KeyCode bind) {
-      this.sound = sound;
+    public Drum(Name name, KeyCode bind) {
+      this.name = name;
       this.bind = bind;
     }
   }
 
-  Drum pata = new Drum(Sound.Pata, KeyCode.F);
-  Drum pon = new Drum(Sound.Pon, KeyCode.A);
-  Drum don = new Drum(Sound.Don, KeyCode.S);
-  Drum chaka = new Drum(Sound.Chaka, KeyCode.D);
+  Drum pata = new Drum(Name.Pata, KeyCode.F);
+  Drum pon = new Drum(Name.Pon, KeyCode.A);
+  Drum don = new Drum(Name.Don, KeyCode.S);
+  Drum chaka = new Drum(Name.Chaka, KeyCode.D);
 
-  void Start() {
-  }
+  void Start() {}
 
   void Update() {
     if (is_hit(pata)) { chant(pata); }
@@ -40,14 +33,14 @@ public class drum : MonoBehaviour {
     if (is_hit(don)) { chant(don); }
     if (is_hit(chaka)) { chant(chaka); }
     
-    if (drums[0].GetComponent<button>().perfect) {
+    if (is_perfect(drums[(int)pata.name])) {
       chant(don);
-      drums[0].GetComponent<button>().perfect = false;
+      drums[(int)pata.name].GetComponent<button>().perfect = false;
     }
   }
 
   void chant(Drum drum) {
-    speaker.PlayOneShot(sounds[(int)drum.sound]);
+    speaker.PlayOneShot(sounds[(int)drum.name]);
   }
 
   void reset_perfect(GameObject drum) {
@@ -56,6 +49,11 @@ public class drum : MonoBehaviour {
 
   bool is_hit(Drum drum) {
     if (Input.GetKeyDown(drum.bind)) { return true; }
+    else { return false; }
+  }
+
+  bool is_perfect(GameObject game_object) {
+    if (game_object.GetComponent<button>().perfect) {return true;}
     else { return false; }
   }
 }
