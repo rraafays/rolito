3e3ca459185e4b3b5937832d3f04bf1046c9d94f
raf <rraf@tuta.io>
2023-04-16 10:29:45 +0100

    update: beat system with no sound

--- Assets/scripts/beat.cs
+++ Assets/scripts/beat.cs
@@ -6,6 +6,7 @@ public class beat : MonoBehaviour
 {
   public float tempo;
   public bool started;
+  public bool can_be_pressed;
 
   void Start() {
     tempo = tempo / 60;
@@ -15,6 +16,18 @@ public class beat : MonoBehaviour
     if(Input.GetKeyDown(KeyCode.Space)) { started = true; }
     if (started) {
       transform.position -= new Vector3(0, tempo * Time.deltaTime, 0);
+    }
+  }
+
+  private void OnTriggerEnter2D(Collider2D other) {
+    if (other.tag == "activator") {
+      can_be_pressed = true;
+    }
+  }
+
+  private void OnTriggerExit2D(Collider2D other) {
+    if (other.tag == "activator") {
+      can_be_pressed = false;
     }
   }
 }
